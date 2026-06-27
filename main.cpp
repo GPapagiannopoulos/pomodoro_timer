5c8ed91291784b5d8f39e03da37a10872a32b21d
George <georgiosmyronpapagiannopoulos@gmail.com>
2026-06-27 10:21:06 +0100

    feat: implement scaffolding for GUI

--- main.cpp
+++ main.cpp
@@ -1,16 +1,62 @@
 #include <iostream>
 #include <thread>
 
+#include <GLFW/glfw3.h>
+#include <imgui.h>
 #include "Session.h"
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 
 int main() {
 	
-	Session test_session = Session(std::chrono::minutes(15), "Test");
+	if (!glfwInit()) {
+		std::cout << "Initialization failed.\n";
+		return 1;
+	}
 
-	std::cout << test_session.getDuration() << "\n";
-	std::cout << test_session.getLabel() << "\n";
-	std::this_thread::sleep_for(std::chrono::seconds(10));
-	auto [hours, minutes, seconds] = test_session.getRemainingTime();
-	std::cout << hours << " " << minutes << " " << seconds << "\n";
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	GLFWwindow* window = glfwCreateWindow(640, 480, "Test", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	
+	ImGui::CreateContext();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 330");
+
+
+	while (!glfwWindowShouldClose(window)) {
+		int duration = 0;
+		int bufSize = 64;
+		char buf[] = "";
+		int h = 1, m = 24, s = 39;
+
+		glfwPollEvents();
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		ImGui::Begin("Timer");
+		ImGui::InputInt("Duration (min)", &duration);
+		ImGui::InputText("Label", buf, bufSize);
+		ImGui::Button("Start");
+		ImGui::Text("Remaining: %d:%02d:%02d", h, m, s);
+		ImGui::End();
+
+		ImGui::Render();
+		glClear(GL_COLOR_BUFFER_BIT);
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		glfwSwapBuffers(window);
+	}
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
 	return 0;
 }
