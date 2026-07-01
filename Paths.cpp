17160457d020e2d0ff9123bdd387e0c9a4d15cf8
George <georgiosmyronpapagiannopoulos@gmail.com>
2026-07-01 22:51:42 +0100

    feat: implement function to find or create user data directories

--- Paths.cpp
+++ Paths.cpp
@@ -5,6 +5,7 @@
 
 #ifdef _WIN32
 #include <windows.h>
+#include <cstdlib>
 std::filesystem::path Paths::executableDir() {
 	std::vector<wchar_t> buffer(MAX_PATH);
 	DWORD charsWritten = GetModuleFileNameW(nullptr, buffer.data(), static_cast<DWORD>(buffer.size()));
@@ -18,10 +19,35 @@ std::filesystem::path Paths::executableDir() {
 	}
 	return std::filesystem::path(buffer.data()).parent_path();
 }
+
+std::filesystem::path Paths::userDataDir(const std::string& appName) {
+	char* appdata = std::getenv("APPDATA");
+	if (!appdata) throw std::runtime_error("Failed to extract app data.");
+	std::filesystem::path dir = std::filesystem::path(appdata) / appName;
+	std::filesystem::create_directories(dir);
+	return dir;
+}
+
 #elif defined(__linux__)
+#include <cstring>
 std::filesystem::path Paths::executableDir() {
 	return std::filesystem::read_symlink("/proc/self/exe").parent_path();
 }
+std::filesystem::path Paths::userDataDir(const std::string& appName) {
+	char* xdgDataHome = std::getenv("XDG_DATA_HOME");
+	std::filesystem::path dir;
+	if (!xdgDataHome || std::strlen(xdgDataHome) == 0) {
+		char* homeDir = std::getenv("HOME");
+		if (!homeDir || std::strlen(homeDir) == 0) throw std::runtime_error("Failed to extract app data.");
+		dir = std::filesystem::path(homeDir) / ".local" / "share" / appName;
+	}
+	else {
+		dir = std::filesystem::path(xdgDataHome) / appName;
+	}
+	std::filesystem::create_directories(dir);
+	return dir;
+}
+
 #elif defined(__APPLE__)
 #include <mach-o/dyld.h>
 std::filesystem::path Paths::executableDir() {
@@ -36,6 +62,12 @@ std::filesystem::path Paths::executableDir() {
 		}
 	} 
 	return std::filesystem::canonical(std::filesystem::path(buffer.data()).parent_path());
-
+}
+std::filesystem::path Paths::userDataDir(const std::string& appName) {
+	char* home = std::getenv("HOME");
+	if (!home) throw std::runtime_error("Failed to get home directory.");
+	std::filesystem::path dir = std::filesystem::path(home) / "Library/Application Support" / appName;
+	std::filesystem::create_directories(dir);
+	return dir;
 }
 #endif
